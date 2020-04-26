1478d2345647d563da54dfc9f3fd34de8e6d0533
Paulo Estêvão <pauloestevao92@gmail.com>
2020-04-26 18:58:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department db = new Department(25,"Pharmacy");
 		Seller seller = new Seller(2500,"Lores","lorenathalia100@gmail.com",new Date(),20000.00,db);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obl);
+	void deleteById(Integer obj);
+	Department findById(Integer id);
+	List<Department> findAll();
 }
