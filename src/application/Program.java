39f4dbf18710426d00ae0fb6f11637f57ed572e6
Paulo Estêvão <pauloestevao92@gmail.com>
2020-04-26 01:14:39 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department db = new Department(25,"Eletronics");
-		System.out.println(db);
+		Department db = new Department(25,"Pharmacy");
+		Seller seller = new Seller(2500,"Lores","lorenathalia100@gmail.com",new Date(),20000.00,db);
+		System.out.println(seller);
 	}
 
 }
